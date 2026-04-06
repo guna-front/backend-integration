475aa71ed17bb8b17236b4db6c866af5c356e52e
Gunavathi <gunavathyd93@gmail.com>
2026-04-06 12:23:09 +0530

    force rebuild settings

--- healthy_project/settings.py
+++ healthy_project/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+print("SETTINGS FILE LOADED")
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
